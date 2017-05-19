e0073eef36602578299956908f89c7f0f88a5158
panshouheng <350621987@qq.com>
2017-05-19 17:30:16 +0800

    Initial Commit

---
+++ ssssaas/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ssssaas
+//
+//  Created by apple on 2017/5/19.
+//  Copyright © 2017年 XiaMenYunYou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ssssaas/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ssssaas
+//
+//  Created by apple on 2017/5/19.
+//  Copyright © 2017年 XiaMenYunYou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
